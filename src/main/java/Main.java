576fc8aeb44a3ede9701b3bf1ad6932fd83a27c3
Millard <millard-barakzai@hotmail.com>
2021-09-20 16:59:14 +0200

    Hej alle en ny opdatering

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello world");
         System.out.println("111111");
-        System.out.println("Suuuup");
+        System.out.println("Suuuasdasdasdadasdup");
     }
 }
