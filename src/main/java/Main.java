1a05667b1a375ea9ade1aeb5e4226470f67ad85c
Elias <s215817@student.dtu.dk>
2021-09-21 15:00:06 +0200

    det er øvelse

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("111111");
         System.out.println("Suuuasdasdasdadasdup");
+        System.out.println("øvelse");
+
     }
 }
