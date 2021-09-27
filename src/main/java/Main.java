cee77887a6d1e3e419c0a035b7273c520a5a5344
Millard <millard-barakzai@hotmail.com>
2021-09-27 14:07:52 +0200

    fejl

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello world");
-        System.out.println("111111");
-        System.out.println("Suuuasdasdasdadasdup");
-        System.out.println("øvelse");
-        System.out.println("Øvelse Millard");
-        System.out.println("virker det?");
-        System.out.println("kom nuuu");
-        System.out.println("qwsadasds<d");
+        System.out.println("Hello worl");
     }
 }
